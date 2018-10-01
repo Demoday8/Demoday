14b40aeecde7e85b248deb48a647bf49b770d8e2
robpradodev <robpradodev@gmail.com>
2018-10-01 17:57:35 -0300

    Configurar backend para os formulários de cadastro

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongo = require('express-mongo-db')
+const cors = require('cors')
+
+app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(mongo('mongodb://localhost/cadastro'));
+
+app.get('/usuarios', (req, res) => {
+    req.db.collection('usuarios').find().toArray((erro, dados) => {
+        if (!erro) {
+            return res.send({ usuarios: dados });
+        }
+        return res.status(500).send({ erro: "Erro ao consultar o banco de dados. Verifique a sua conexão com a Internet" });
+    })
+})
+
+app.post('/usuario/novo', (req, res) => {
+    if (!req.body.nome || !req.body.email || !req.body.senha) {
+        return res.status(400).send({ erro: "Parâmetros obrigatórios ausentes" });
+    }
+
+    req.db.collection('usuarios').insert(req.body, erro => {
+        if (!erro) {
+            return res.send({ mensagem: "Usuário cadastrado" });
+        }
+        return res.status(500).send({ erro: "Problema ao consultar o banco de dados" });
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Serviço de cadastro está sendo executado");
+});
+
