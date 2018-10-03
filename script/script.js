044a73d52911240c5e21bfce11876e91f3786b76
robpradodev <robpradodev@gmail.com>
2018-10-03 19:06:34 -0300

    Solucionado o erro de captura de dados do formulário :D

--- script/script.js
+++ script/script.js
@@ -1,4 +1,24 @@
-const form = document.getElementsByClassName('.formulario-cadastro')[0];
 
-const data = getFormDataAsJSON(form);
+// const data = getFormDataAsJSON(form);
+
+
+
+
+const formToJSON = elements => [].reduce.call(elements, (data, element) =>{
+    data[element.name] = element.value;
+    return data;
+}, {});
+
+const handleFormSubmit = event => {
+
+    event.preventDefault();
+
+    const data = formToJSON(form.elements);
+
+    console.log(data);
+};
+
+const form = document.getElementsByClassName('formulario-cadastro')[0];
+
+form.addEventListener('submit', handleFormSubmit);
 
