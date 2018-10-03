6800d6fa772b3bc371948ebf3c53fdbd0b472f00
robpradodev <robpradodev@gmail.com>
2018-10-03 09:22:06 -0300

    Capturar o formulário e declarar variável para converter os dados do formulário para JSON

--- script/script.js
+++ script/script.js
@@ -1,29 +1,4 @@
-document.querySelector('.enviar-cadastro').addEventListener('click', Cadastrar);
+const form = document.getElementsByClassName('.formulario-cadastro')[0];
 
+const data = getFormDataAsJSON(form);
 
-Cadastrar('http://localhost:5000/usuario/novo')
- .then(response => console.log(response.json()))
- .then(data => console.log(data))
- .catch(error => console.log(error));
-
- function Cadastrar(url) {
-   const formDados = new FormData(document.querySelector('.formulario-cadastro'))
-
-   /* let jsonFormDados = {}
-
-   for (let [key, value] of formDados.entries()){
-       console.log(formDados)
-       jsonFormDados[key]=value
-   } */
-
-   console.log(formDados);
-
-   return fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(formDados),
-        headers: {
-            "Content-Type": "application/json"
-        }
-   })
-
- };
